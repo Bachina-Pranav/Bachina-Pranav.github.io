813977c7506977d92950928148ebf2bd7b4834e1
Bachina-Pranav <Bachina-Pranav@users.noreply.github.com>
2026-09-02 18:42:24 +0000

    Deploying to gh-pages from @ Bachina-Pranav/Bachina-Pranav.github.io@5c08eac0d12ad5927e5f6220d8ba42701508de2c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,18 +398,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-c-shell",
+            },},{id: "projects-c-shell",
           title: 'C-SHELL',
           description: "A Linux command-line shell with process, signal, pipe, and redirection support.",
           section: "Projects",handler: () => {
